7ddeecf8c514360cbf0c096254a882eb8b11a862
Nick Quillen <nicktquillen@gmail.com>
2023-07-27 15:33:21 -0400

    Initial project setup from Spring initializr

---
+++ src/test/java/com/nquillen/vetclinic/VetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nquillen.vetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
