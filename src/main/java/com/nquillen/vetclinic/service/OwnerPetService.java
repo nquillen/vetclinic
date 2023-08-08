c0e8be013a3315842b5935018dc1a1c3ce3c50da
Nick Quillen <nicktquillen@gmail.com>
2023-08-08 18:42:45 -0400

    started adding service methods

--- src/main/java/com/nquillen/vetclinic/service/OwnerPetService.java
+++ src/main/java/com/nquillen/vetclinic/service/OwnerPetService.java
@@ -1,7 +1,34 @@
 package com.nquillen.vetclinic.service;
 
+import com.nquillen.vetclinic.Entity.Owner;
+import com.nquillen.vetclinic.Entity.Pet;
 import org.springframework.stereotype.Service;
 
-@Service
-public interface OwnerService {
+import java.util.List;
+
+
+public interface OwnerPetService {
+
+    List<Owner> findAllOwners();
+
+    Owner findOwnerAndPetsByOwnerId(int id);
+
+    Pet findPetByName(String name);
+
+    Owner saveOwner(Owner owner);
+
+    Pet savePet(Pet pet);
+
+    void deleteOwnerById(int id);
+
+    void deletePetById(int id);
+
+    Owner findByOwnerName(String name);
+
+    Owner findByPhoneNumber(String phoneNumber);
+
+
+
+
+
 }
--- src/main/java/com/nquillen/vetclinic/service/OwnerPetServiceImpl.java
+++ src/main/java/com/nquillen/vetclinic/service/OwnerPetServiceImpl.java
@@ -1,2 +1,60 @@
-package com.nquillen.vetclinic.service;public class OwnerPetServiceImpl {
+package com.nquillen.vetclinic.service;
+
+import com.nquillen.vetclinic.Entity.Owner;
+import com.nquillen.vetclinic.Entity.Pet;
+import com.nquillen.vetclinic.dao.OwnerRepository;
+import com.nquillen.vetclinic.dao.PetRepository;
+
+import java.util.List;
+
+public class OwnerPetServiceImpl implements OwnerPetService{
+
+    private OwnerRepository ownerRepository;
+
+    private PetRepository petRepository;
+
+    @Override
+    public List<Owner> findAllOwners() {
+        return ownerRepository.findAll();
+    }
+
+    @Override
+    public Owner findOwnerAndPetsByOwnerId(int id) {
+        return null;
+    }
+
+    @Override
+    public Pet findPetByName(String name) {
+        return petRepository.find;
+    }
+
+    @Override
+    public Owner saveOwner(Owner owner) {
+        return null;
+    }
+
+    @Override
+    public Pet savePet(Pet pet) {
+        return null;
+    }
+
+    @Override
+    public void deleteOwnerById(int id) {
+
+    }
+
+    @Override
+    public void deletePetById(int id) {
+
+    }
+
+    @Override
+    public Owner findByOwnerName(String name) {
+        return null;
+    }
+
+    @Override
+    public Owner findByPhoneNumber(String phoneNumber) {
+        return null;
+    }
 }
