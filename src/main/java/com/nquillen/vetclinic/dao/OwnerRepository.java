1e06ed9ca00f793b464d03a52063d252b02f7b23
Nick Quillen <nicktquillen@gmail.com>
2023-08-10 14:59:03 -0400

    added derived query methods to repositories

--- src/main/java/com/nquillen/vetclinic/dao/OwnerRepository.java
+++ src/main/java/com/nquillen/vetclinic/dao/OwnerRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
+    List<Owner> findByName(String name);
+
+    Owner findByPhoneNumber(String phoneNumber);
+
+    Owner findByEmail(String email);
 
 }
